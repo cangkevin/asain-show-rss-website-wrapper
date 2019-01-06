f11dc9e1316e26aff9cb149b8d183ab9e5ceadb0
Kevin Cang <cangkevin@gmail.com>
2019-01-06 14:32:26 -0500

    removed unused dependencies

--- website/core.py
+++ website/core.py
@@ -2,7 +2,7 @@ import functools
 
 from .rss_client import RSSClient
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from . import const
 
