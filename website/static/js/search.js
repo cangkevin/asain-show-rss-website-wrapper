1d7cd44bac50cbc36be357ab0d1fc9c029bfd8bb
cangkevin <cangkevin@gmail.com>
2020-01-01 16:49:31 -0600

    refactor: consolidate vue components
    Move Vue components to be more self-contained for modularity and maintenance purposes
    
    closes #73

--- website/static/js/search.js
+++ website/static/js/search.js
@@ -1,20 +1,19 @@
-var searchApp = new Vue({
-  el: '#search',
-  data() {
+Vue.component('search', {
+  data: function() {
     return {
       selected: false,
       query: '',
       results: []
     }
   },
+  created: function() {
+    this.debouncedQueryIndex = _.debounce(this.queryIndex, 750)
+  }, 
   watch: {
     query: function(newQuery) {
       this.debouncedQueryIndex()
     }
   },
-  created: function() {
-    this.debouncedQueryIndex = _.debounce(this.queryIndex, 750)
-  }, 
   methods: {
     queryIndex: function() {
       if (this.query) {
@@ -39,5 +38,41 @@ var searchApp = new Vue({
       }
     }
   },
-  delimiters: ['[[',']]']
+  template:`
+    <form v-on:submit.prevent>
+      <div class="row">
+        <div class="input-group col">
+          <input v-model.trim="query"
+                 v-on:focus="showSuggestions"
+                 v-on:blur="hideSuggestions"
+                 class="form-control form-control-sm border-right-0 border"
+                 type="search" placeholder="Search...">
+          <span class="input-group-append">
+            <div class="input-group-text bg-white">
+              <i class="fa fa-search"></i>
+            </div>
+          </span>
+        </div>
+      </div>
+
+      <div id="suggestions" 
+           class="position-absolute list-group"
+           v-bind:class="{'d-none': !selected }">
+        <template v-for="result in results">
+          <a :href="'/episodes/' + result.id + '/1'"
+              class="list-group-item list-group-item-action suggestion"
+              v-bind:key="result.id">
+            <div class="media">
+              <img :src="result.image" class="mr-3" />
+              <div class="media-body align-self-center">
+                <h6>{{ result.title }}</h6>
+              </div>
+            </div>
+          </a>
+        </template>
+      </div>
+    </form>
+    `
 })
+
+var searchApp = new Vue({ el: '#search' })
