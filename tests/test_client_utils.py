eb031b23ea169c06ef5602ff39a5e4efbabf7385
cangkevin <cangkevin@gmail.com>
2020-04-26 19:22:11 -0500

    style: format test code
    Reformatted test code to achieve code style consistency.

--- tests/test_client_utils.py
+++ tests/test_client_utils.py
@@ -3,13 +3,11 @@ import feedparser
 from pathlib import Path
 
 from website.client import utils
-from website.const import (
-    MOVIES_RESP_FILE, EPISODES_RESP_FILE, SOURCES_RESP_FILE
-)
+from website.const import MOVIES_RESP_FILE, EPISODES_RESP_FILE, SOURCES_RESP_FILE
 
 
 def test_extract_paginations_with_one_present():
-    with open(Path(MOVIES_RESP_FILE), 'rb') as data:
+    with open(Path(MOVIES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         entries = utils.extract_show_or_movie_entries(rss_data)
         paginations = utils.extract_paginations(entries)[1]
@@ -18,7 +16,7 @@ def test_extract_paginations_with_one_present():
 
 
 def test_extract_paginations_with_none_present():
-    with open(Path(SOURCES_RESP_FILE), 'rb') as data:
+    with open(Path(SOURCES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         entries = utils.extract_episodes(rss_data)
         paginations = utils.extract_paginations(entries)[1]
@@ -27,7 +25,7 @@ def test_extract_paginations_with_none_present():
 
 
 def test_extract_show_or_movie_entries():
-    with open(Path(MOVIES_RESP_FILE), 'rb') as data:
+    with open(Path(MOVIES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         entries = utils.extract_show_or_movie_entries(rss_data)
 
@@ -35,7 +33,7 @@ def test_extract_show_or_movie_entries():
 
 
 def test_extract_episodes():
-    with open(Path(EPISODES_RESP_FILE), 'rb') as data:
+    with open(Path(EPISODES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         entries = utils.extract_episodes(rss_data)
         episodes = utils.extract_paginations(entries)[0]
@@ -44,17 +42,17 @@ def test_extract_episodes():
 
 
 def test_extract_picture():
-    with open(Path(MOVIES_RESP_FILE), 'rb') as data:
+    with open(Path(MOVIES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         test_entry = rss_data.entries[0]
         picture_url = utils.extract_picture(test_entry)
 
         assert isinstance(picture_url, str)
-        assert '.jpg' in picture_url
+        assert ".jpg" in picture_url
 
 
 def test_extract_id():
-    with open(Path(MOVIES_RESP_FILE), 'rb') as data:
+    with open(Path(MOVIES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         test_entry = rss_data.entries[0]
         entry_id = utils.extract_id(test_entry)
@@ -63,7 +61,7 @@ def test_extract_id():
 
 
 def test_extract_sources():
-    with open(Path(SOURCES_RESP_FILE), 'rb') as data:
+    with open(Path(SOURCES_RESP_FILE), "rb") as data:
         rss_data = feedparser.parse(data)
         entries = utils.extract_sources(rss_data)
 
